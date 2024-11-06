6661e3b3be3c55d56b7b09f2b0b24e83fb964356
robouvier2 <cipix10120@proton.me>
2024-11-06 15:07:20 +0100

    oe tkt j'ai changé les trucs là

--- app/src/main/java/com/example/pikter/LoginActivity.java
+++ app/src/main/java/com/example/pikter/LoginActivity.java
@@ -1,4 +1,23 @@
 package com.example.pikter;
 
-public class LoginActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.login);
+    }
+
+    public void onSignUpClicked(View view) {
+        // Créer un intent pour démarrer SignupActivity
+        Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+        startActivity(intent); // Lancer l'activité
+    }
 }
--- app/src/main/java/com/example/pikter/SignupActivity.java
+++ app/src/main/java/com/example/pikter/SignupActivity.java
@@ -1,4 +1,23 @@
 package com.example.pikter;
 
-public class SignupActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inscription);
+    }
+
+    public void onLoginClicked(View view) {
+        // Créer un intent pour démarrer LoginActivity
+        Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+        startActivity(intent); // Lancer l'activité
+    }
 }
